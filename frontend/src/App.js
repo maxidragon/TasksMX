16c7e9455d4eaf2d934712ba809a6b9cb2716a85
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-02-07 20:25:42 +0100

    Add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Tasks from "./components/Tasks/Tasks";
+import Footer from "./components/UI/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Tasks />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Tasks/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/UI/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="appName">
+                TasksMX
+            </div>
+            <div className="author">
+                Made with ♥ by maxidragon
+            </div>
+            <div className="icons">
+                <a href="https://github.com/maxidragon/TasksMX">
+                    
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
