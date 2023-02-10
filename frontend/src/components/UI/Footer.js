f4ce8b270a584933aebd5d1c402b9ebe4637940c
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-02-10 19:40:22 +0100

    Some css again and footer

--- frontend/src/components/UI/Footer.js
+++ frontend/src/components/UI/Footer.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import './Footer.css';
+import {IconButton, Typography} from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="appName">
+            <Typography variant="h6" align="center" gutterBottom>
                 TasksMX
-            </div>
-            <div className="author">
-                Made with ♥ by maxidragon
-            </div>
-            <div className="icons">
-                <a href="https://github.com/maxidragon/TasksMX">
+            </Typography>
+            Made with ♥ by maxidragon
+            <div>
 
-                </a>
-                <a href="mailto:maksymilian.gala1@gmail.com">
-
-                </a>
             </div>
+            <IconButton aria-label="GitHubIcon" href="https://github.com/maxidragon/TasksMX" target="_blank">
+                <GitHubIcon sx={{color: "#fff"}} fontSize="large"/>
+            </IconButton>
+            <IconButton aria-label="MailIcon" href="mailto:contact@mgala.ml" target="_blank">
+                <MailIcon sx={{color: "#fff"}} fontSize="large"/>
+            </IconButton>
         </footer>
     )
 }
